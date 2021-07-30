954555c6cfbfc92359993ecbbac01d5340eceb2f
Akhmadjon <formyproject1999@gmail.com>
2021-07-30 14:53:29 +0500

    Initial Create

---
+++ GrpcExtention/GrpcCallerService.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcExtantion
+{
+    public static class GrpcCallerService
+    {
+        public static async Task<TResponse> CallService<TResponse>(string urlGrpc, Func<GrpcChannel, Task<TResponse>> func)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+
+            var channel = GrpcChannel.ForAddress(urlGrpc);
+            try
+            {
+                return await func(channel);
+            }
+            catch (RpcException e)
+            {
+                return default;
+            }
+            finally
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            }
+        }
+
+        public static async Task CallService(string urlGrpc, Func<GrpcChannel, Task> func)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+
+            var channel = GrpcChannel.ForAddress(urlGrpc);
+
+            try
+            {
+                await func(channel);
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            }
+        }
+    }
+}
---
+++ GrpcServer/GrpcService/GrpcService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using GrpcService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.GrpcService
+{
+    public class BaseGrpcService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply() { Message = "Hello " + request.Name });
+        }
+    }
+}
